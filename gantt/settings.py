345b80ed965a23bd92ee0c7e0a0c74f43ab03880
kndbvortex <kamganguifo@gmail.com>
2022-07-31 00:19:28 +0100

    Fix Front-end view

--- gantt/settings.py
+++ gantt/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 print(os.environ.get('DEBUG'))
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.80.185", "127.0.0.1"]
 
 
 # Application definition
